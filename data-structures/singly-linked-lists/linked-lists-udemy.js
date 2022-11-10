0c9ff4ae2923349f0260b122582a17f6763787d8
Cherry Xu <ms.cherry.xu@gmail.com>
2022-11-10 12:23:16 -0500

    Unshift method on LinkedList

--- data-structures/singly-linked-lists/linked-lists-udemy.js
+++ data-structures/singly-linked-lists/linked-lists-udemy.js
@@ -18,6 +18,16 @@ Popping pesudocode
 - Return the value of the node removed
 */
 
+/* Unshifting pseudocode
+- This function should accept a value
+- Create a new node using the value passed to the function
+- If there is no head property on the list, set the head and tail to be the newly created node
+- Otherwise set the newly created node's next property to be the current head property on the list
+- Set the head property on the list to be that newly created node
+- Increment the length of the list by 1
+- Return the linked list
+*/
+
 class SinglyLinkedList {
   constructor() {
     this.head = null;
@@ -66,6 +76,22 @@ class SinglyLinkedList {
     }
     return currentHead;
   }
+
+  unshift(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 const list = new SinglyLinkedList();
+list.push("hello");
+list.push("goodbye");
+list.push("!");
